3091065af50c2126e047d5a1d34133eb5de83108
Tatiana <kutuzova.tanya@mail.ru>
2019-01-21 18:52:28 +0300

    h2 init task

--- hw2/src/src.js
+++ hw2/src/src.js
@@ -1,23 +1,5 @@
 var col_to_show = ['name', 'continent', 'gdp', 'life_expectancy', 'population', 'year'];
 var sorting_flag = false;
-var format_data = function(row){
-	  return col_to_show
-	  		.map(function(column) {
-				var formatComma = d3.format(","),
-				formatDecimal = d3.format(".1f"),
-				formatSI = d3.format('.3s');
-				if(column == "gdp"){
-					return formatSI(row[column]);
-				}
-				if(column == "life_expectancy"){
-					return formatDecimal(row[column]);
-				}
-				if(column == "population"){
-					return formatComma(row[column]);
-				}
-	  			return row[column];
-	  		})
-	  	};
 
       d3.json("https://raw.githubusercontent.com/tatiasha/tatiasha.github.io/master/hw2/data/countries_2012.json", function(error, data){
 
@@ -37,11 +19,11 @@ var format_data = function(row){
           .on("click", function(header, i) {
             tbody.selectAll("tr").sort(function(a, b) {
               if (sorting_flag == false){
-                return d3.descending(a[header], b[header]);
-                sorting_flag = true;
-              }else{
+								sorting_flag = true;
                 return d3.ascending(a[header], b[header]);
-                sorting_flag = false;
+              }else{
+								sorting_flag = false;
+                return d3.descending(a[header], b[header]);
               }
             });
           });
@@ -52,18 +34,25 @@ var format_data = function(row){
           .append("tr").attr("class", "row");
 
           var cells = rows.selectAll("td")
-          	.data(format_data)
+          	.data(function(row){
+							  return col_to_show
+							  		.map(function(column) {
+										var formatComma = d3.format(","),
+										formatDecimal = d3.format(".1f"),
+										formatSI = d3.format('.3s');
+										if(column == "gdp"){
+											return formatSI(row[column]);
+										}
+										if(column == "life_expectancy"){
+											return formatDecimal(row[column]);
+										}
+										if(column == "population"){
+											return formatComma(row[column]);
+										}
+							  			return row[column];
+							  		})
+							  	})
                 .enter()
                 .append("td")
                 .text(function(d) { return d;});
-        // var cells = rows.selectAll("td")
-        //   .data(function(row) {
-        //       return d3.range(Object.keys(row).length).map(function(column, i) {
-        //           return row[Object.keys(row)[i]];
-        //       });
-        //   })
-        //   .enter()
-        //   .append("td")
-        //   .text(function(d) { return d; })
-
       });
